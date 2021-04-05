0647b0f3e0de95f4785635d02e6e96dd253e32c5
ByDylan-YH <921644606@qq.com>
2021-04-05 23:52:42 +0800

    better

--- src/main/java/mybatis/dao/UserDao.java
+++ src/main/java/mybatis/dao/UserDao.java
@@ -36,31 +36,31 @@ import java.util.List;
 public interface UserDao {
 
     //    查询所有用户
-    @Select("select * from user")
+//    @Select("select * from user")
     List<User> findAll();
 
     //    保存用户
-    @Insert("insert into user(username,address,sex,birthday)values(#{username},#{address},#{sex},#{birthday})")
+//    @Insert("insert into user(username,address,sex,birthday)values(#{username},#{address},#{sex},#{birthday})")
     void saveUser(User user);
 
     //    更新用户
-    @Update("update user set username=#{username},sex=#{sex},birthday=#{birthday},address=#{address} where id=#{id}")
+//    @Update("update user set username=#{username},sex=#{sex},birthday=#{birthday},address=#{address} where id=#{id}")
     void updateUser(User user);
 
     //    删除用户
-    @Delete("delete from user where id=#{id} ")
+//    @Delete("delete from user where id=#{id}")
     void deleteUser(Integer userId);
 
     //    根据id查询用户
-    @Select("select * from user where id=#{id} ")
+//    @Select("select * from user where id=#{id}")
     User findById(Integer userId);
 
     //    根据id查询用户
-    @Select("select * from user where id=#{id} ")
+    @Select("select * from user where id=#{id}")
     User findById2(@Param("id") Integer userId);
 
     //    根据用户名称模糊查询,string 类型 用 $,不能用 #
-    @Select("select * from user where username like '%${value}%';")
+    @Select("select * from user where username like '%${value}%'")
     List<User> findUserByName(String username);
 
     //    查询总用户数量
